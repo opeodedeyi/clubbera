ad8472bfb087c8cad347cb32f674ce91961d5913
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-05-21 21:07:08 +0100

    more updates to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,14 +23,20 @@ const Home = () => {
 
             <div className="home-intro">
                 <div className="home-intro-text">
-                    <h1 className="home-intro-header">Connect <br /> Engage & Thrive <br /> With <span className="home-intro-header-special">Clubbera</span></h1>
+                    <h2 className="home-intro-header">Connect <br /> Engage & Thrive <br /> With <span className="home-intro-header-special">Clubbera</span></h2>
                     <p className="home-intro-subtitle">Welcome to Clubbera - Where Connections Come Alive! Clubbera is a modern online platform that brings people together for engaging social experiences.</p>
                     <CustomButton screentype="none" onClick={signupButtonClick}>Join Clubbera</CustomButton>
                 </div>
-                
+
+                {/* image might be here */}
             </div>
 
-            <div className="home-"></div>
+            {/* <div className="home-">we need a CTA to create a group</div> */}
+
+            <div className="home-easyuse">
+                <h2 className="home-easyuse-title">Easy to <span className="home-intro-header-special">Use</span></h2>
+                <p className="home-easyuse-subtext">Form new connections with individuals who have common interests through a wide range of physical clubs. Joining our platform is easy to use and absolutely free.</p>
+            </div>
 
         </>
     )
