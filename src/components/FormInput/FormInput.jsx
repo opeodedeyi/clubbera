c32dc970f128d98fbce7dc059d5c2535975865fd
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-05-25 11:49:11 +0100

    done some forms

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -4,28 +4,23 @@ import "./FormInput.css";
 const FormInput = ( props ) => {
     if (props.type === 'textarea') {
         return (
-            <>
-                <label className="form-label">{ props.input }</label>
-                <textarea
-                    className="form-input form-textarea"
-                    placeholder={props.placeholder}
-                    value={props.value}
-                    onChange={props.onChange}
-                ></textarea>
-            </>
+            <textarea
+                className="form-input form-textarea"
+                placeholder={props.placeholder}
+                value={props.value}
+                onChange={props.onChange}
+            ></textarea>
         )
     } else {
         return (
-            <>
-                <label className="form-label">{ props.input }</label>
+            <div className="form-input-normal">
                 <input
-                    className="form-input"
                     type={props.type}
                     placeholder={props.placeholder}
                     value={props.value}
                     onChange={props.onChange}
                 />
-            </>
+            </div>
         );
     }
 }
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "../../components/header/Header";
+import FormInput from "../../components/FormInput/FormInput";
 import './Signup.css';
 
 const Signup = () => {
@@ -7,9 +8,60 @@ const Signup = () => {
         document.title = "Sign up | Clubbera";
     }, []);
 
+    const [fullname, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const isEmailValid = (email) => {
+        const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+        return emailRegex.test(email);
+    };
+
+    const isPasswordValid = (password) => {
+        // At least one, At least one lowercase, At least one uppercase, At least one special, A total of at least 8 characters
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+        return passwordRegex.test(password);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(fullname, email, password);
+    };
+
+    const isDisabled = !fullname || !email || !password || !isEmailValid(email) || !isPasswordValid(password);
+    
     return (
         <>
             <Header />
+
+            <div className="signup-container">
+                <div className="signup-container-content">
+                    <form action=""  className="signup-container-main">
+                        <p className="signup-container-main-title">Create an Account</p>
+                        <p className="signup-container-main-subtitle">Events are happening every day — sign up to join the fun</p>
+                        
+                        <FormInput
+                            type="text"
+                            placeholder="Full name" 
+                            input="Your name"
+                            value={fullname}
+                            onChange={(e) => setFullName(e.target.value)}/>
+
+                        <FormInput
+                            type="email"
+                            placeholder="Email" 
+                            input="Your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}/>
+
+                        
+                    </form>
+                </div>
+            </div>
+
+            <div className="signup-toolkit">
+
+            </div>
         </>
     )
 }
