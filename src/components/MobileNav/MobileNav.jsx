b76fc5de8721f1ed8305d18702e6d9860280caad
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-07-18 15:42:17 +0100

    mobile nav

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -12,18 +12,41 @@ const MobileNav = (props) => {
 
     return (
         <div className={`mobile-nav ${isOpen ? "open" : ""}`}>
-            <button className="mobile-nav-toggle" onClick={onCancel}>
-                <img src={cancel} alt="cancel" className="mobile-nav-cancel-icon"/>
-            </button>
+            <div className="mobile-nav-header">
+                <button className="mobile-nav-toggle" onClick={onCancel}>
+                    <img src={cancel} alt="cancel" className="mobile-nav-cancel-icon"/>
+                </button>
+
+                <p className="mobile-nav-header-text">Hi there,</p>
+            </div>
+
+            {user ? 
+                    <NavLink to={`/pr/${user.uniqueURL}`} className="mobile-nav-profile-container">
+                        {user?.profilePhoto?.location ? (
+                            <img src={user.profilePhoto.location} alt="profile"/>
+                        ) :
+                            <img src="https://via.placeholder.com/50" alt="profile"/>
+                        }
+
+                        <p className="mobile-nav-profile-name">{user.fullname}</p>
+                    </NavLink>
+            :
+                null
+            }
+            
             <nav className="mobile-nav-menu">
+
                 <ul className="mobile-nav-menu-list">
                     {user ? 
                         <>
                             <li>
-                                <NavLink to="/createclub" onClick={onCancel}>Create { import.meta.env.VITE_APP_THEME }</NavLink>
+                                <NavLink to="/createclub" onClick={onCancel}>Create a { import.meta.env.VITE_APP_THEME }</NavLink>
                             </li>
                             <li>
-                                <p onClick={logoutClick}>Log Out</p>
+                                <NavLink to="/managecommunity" onClick={onCancel}>Manage your { import.meta.env.VITE_APP_THEME }</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/seecommunity" onClick={onCancel}>See created Communities</NavLink>
                             </li>
                         </>
                     :
@@ -35,6 +58,21 @@ const MobileNav = (props) => {
                                 <NavLink to="/login" onClick={onCancel}>Log In</NavLink>
                             </li>
                         </>
+                    }
+                     <li>
+                        <NavLink to="/hellp" onClick={onCancel}>Help</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" onClick={onCancel}>About</NavLink>
+                    </li>
+                    {user ? 
+                        <>
+                            <li>
+                                <p onClick={logoutClick}>Log Out</p>
+                            </li>
+                        </>
+                    :
+                        null
                     }
                 </ul>
             </nav>
